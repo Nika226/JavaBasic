047d019e4b4a807553277aafc4df6cca7c06eefd
Veronika <veronika9ds114@gmail.com>
2022-09-13 16:04:24 +0200

    Create DomZadanie11.java

---
+++ DomZadanie11.java
@@ -0,0 +1,19 @@
+/**
+* Java Basic. Home work #1
+*
+*@author Veronika
+*@11.09-2022
+*
+*/
+
+class DomZadanie11 {
+    public static void main(String[] args) {
+        // home1
+    System.out.println(canWalk(true, true)? "I can walk" : "I cannot walk");
+    System.out.println(canWalk(false, true)? "I can walk" : "I cannot walk");
+    System.out.println(canWalk(true, false)? "I can walk" : "I cannot walk");
+    System.out.println(canWalk(false, false)? "I can walk" : "I cannot walk");
+    
+        
+        }
+}

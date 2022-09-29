abc50251438a325805ab38730af2e4659bf96715
Veronika <112717708+Nika226@users.noreply.github.com>
2022-09-29 22:10:37 +0200

    Add files via upload

--- DomZadanie2.java
+++ DomZadanie2.java
@@ -3,85 +3,64 @@
 *
 *@author Veronika
 *@todo 07.09-2022
-*@date 13-25.09-2022
+*@date 13-29.09-2022
 */
 
+
 class DomZadanie2 {
-    public static void main(String[] args) {      
-    // home1
-    int t = -210;
-    boolean booleanExpression = t < 0;
-    System.out.println(booleanExpression);
-    if (booleanExpression) {
-        System.out.println("is frost");
-    } else {
-        System.out.println("is no frost");    
-        }
-        
-    t = 11;
-    boolean booleanExpression2 = t > 0 || t == 18;
-    System.out.println(booleanExpression2);
-    if (booleanExpression) {
-        System.out.println("is cold");
-    } else {
-        System.out.println("is no cold");    
-        }
-        
-    t = 20;
-    boolean booleanExpression3 = t > 0 || t == 28;
-    System.out.println(booleanExpression3);
-    if (booleanExpression) {
-        System.out.println("is warm");
-    } else {
-        System.out.println("is no warm");    
-                
-    t = 30;
-    boolean booleanExpression4 = t > 29;
-    System.out.println(booleanExpression4);
-    if (booleanExpression) {
-        System.out.println("is hot");
-    } else {
-        System.out.println("is no hot"); 
-    }  
-    
-      // home2
+     // home1 
+     static String checkWeather (int temperature){     
+             if(temperature <= -210) { 
+             return "It's veryfrost"; 
+         } else if (temperature <= 18) { 
+             return "It's cold"; 
+         } else if (temperature <= 28) { 
+             return "It's warm"; 
+         } else { 
+             return "It's hot";          
+         }   
+         }
+         // home2
+         static Boolean canWalk(Boolean isWeekend, Boolean isWork) {          
+         
+         if(isWeekend && !isWork) {
+             return true; 
+         } else { 
+             return false; 
+         }    
+     }
+          // home3
+      static int getAddition(int x, int y) { 
+         int zet = x + y; 
+         return zet; 
+     } 
+     static int getSubtraction(int x, int y) { 
+         int key = x - y; 
+         return key; 
+     } 
+     static int getMultiplication(int x, int y) { 
+         int jey = x * y; 
+         return jey; 
+     } 
+     static double getDivision(int x, int y) { 
+         double si = (double) x / y; 
+         return si; 
+     }      
+         public static void main(String[] args) {
+         int x = 6; 
+         int y = 3; 
+         int temperature = 0;
+         Boolean isWeekend = true;          
+         Boolean isWork = false;               
+         System.out.println(canWalk(isWeekend, isWork));      
+         System.out.println(checkWeather(temperature));         
+         System.out.println(getAddition(x, y)); 
+         System.out.println(getSubtraction(x, y)); 
+         System.out.println(getMultiplication(x, y)); 
+         System.out.println(getDivision(x, y));     
+    }
+}
 
-    public static void Walkary(isWeekend, isAmFree, isRain, isWork){  
-    
-    if (isWeekend && isAmFree) {
-         System.out.println("I can walk");            
-        } else {
-        System.out.println("I cannot walk");
-        }
-        
-    if (isWork && isRain) {
-         System.out.println("I cannot walk");            
-        } else {
-        System.out.println("I can walk");
-   }
-   // home3
-     
-    public static integer myMethod(int a, int b) { 
-        int d = a + c;    
-        return d;       
-     }       
-        int m = a - c;
-        return m;
-     }        
-        int g = a * c;
-        return g;
-     }    
-        int p = a * c;
-        return p;
-     }     /** pochemu snova rugaetsay kompillaytor? Dlay kajdogo zadaniay ay realiyovala otdelniy metod, kak Vi skayali.
-     I vse ravno oshibka(
-    */
-   }
- }
-
-  
-  
- 
 
 
 

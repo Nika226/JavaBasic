56519eda41ccca96cbb37d30664cfa56fdd3a7f7
Veronika <112717708+Nika226@users.noreply.github.com>
2022-10-11 23:36:51 +0200

    Add files via upload

---
+++ DomZadanie9.java
@@ -0,0 +1,29 @@
+import lesson9.Cats;
+import lesson9.Dogs;
+import lesson9.IAnimal;
+
+/**
+ * Java Basic. Home work #88
+ *
+ *@author Veronika
+ *@todo 05.10.2022
+ *@date 06-11.10-2022
+ *
+ */
+
+public class DomZadanie9 {
+    public static void main(String[] args) {
+        Cats cat = new Cats("Patrik", "white", 4);
+        Dogs dog = new Dogs("Olaf", "black", 2);
+        IAnimal[] animals = {cat, dog};
+
+        for (IAnimal animal : animals) {
+            System.out.println(animal);
+            System.out.println(animal.voice());
+        }
+        System.out.println(cat);
+        System.out.println(cat.voice());
+
+    }
+
+}
